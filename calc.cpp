7e6314d24c6c33017eafd5f30f0e91e1c3dcfb64
nehal-gaikwad <nehal.gaikwad006@gmail.com>
2017-07-19 14:31:11 +0530

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -3,12 +3,12 @@ using namespace std;
 void main()         //program execution start from here
 {
 
-cout<<"program for calculator "<<endl;
+cout<<"program for calculator "<<endl
 int a,b,ans;
 cout<<"1)addition"<<endl;
 cout<<"enter values for a and b"<<endl;
 cin>>a>>b;
-ans=a+b;
+ans=a+b:
 cout<<"addition ="<<ans;
 
 cout<<"2)subtraction"<<endl;
